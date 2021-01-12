eb0158532eebd905ba5888097f8d40e17a9f0cf3
KriszProg <krisz.codecool@gmail.com>
2021-01-12 06:55:31 +0100

    create some 'Stuff' and save them to DB when App start

--- stuff-service/src/main/java/com/codecool/stuffservice/StuffServiceApplication.java
+++ stuff-service/src/main/java/com/codecool/stuffservice/StuffServiceApplication.java
@@ -1,13 +1,42 @@
 package com.codecool.stuffservice;
 
+import com.codecool.stuffservice.service.DBInitializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StuffServiceApplication {
+
+
+	private final DBInitializer dbInitializer;
+
+	@Autowired
+	public StuffServiceApplication(DBInitializer dbInitializer) {
+		this.dbInitializer = dbInitializer;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StuffServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner init() {
+		return args -> {
+			dbInitializer.initDB();
+		};
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
