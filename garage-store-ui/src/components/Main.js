5c87cf37a047162631f60a6dad96d9c4ea890da0
AnettKeszler <anett.dr.keszler@gmail.com>
2021-01-12 17:43:54 +0100

    get data from '/stuff/all' api endpoint with axios useEffect

--- garage-store-ui/src/components/Main.js
+++ garage-store-ui/src/components/Main.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { Link} from 'react-router-dom';
+import Axios from 'axios';
 
-function Main() {
+
+function Main(props) {
+    const [stuffs, setStuffs] = useState([{name: "loading..."}]);
+
+    useEffect(() => {
+        Axios.get('http://localhost:8081/stuff/all').then(({data}) => {
+            setStuffs(data);
+            console.log(data);
+            }
+        );
+    },[]);
+
     return(
         <div style={main}>
             <h1>GARAGE STORE</h1>
+            <div>
+                {stuffs.map(stuff => {
+                    const {id, name, image, price } = stuff;
+                    return (
+                        <div>
+                            <Link to={{pathname: `/stuff/${id}`}}>
+                                 {name}
+                            </Link>
+                            <div>{price}$</div>
+                            <div>{ image }</div>
+                        </div>
+                    );
+                })
+                }
+            </div>
         </div>
     );
 }
