688551c92e01b089f85fc4a589f5435a0116badd
Eszkis <laszlo.szkis@gmail.com>
2021-01-12 11:44:00 +0100

    add new REST api (stuff/{stuff_id}) endpoint for detail one specific stuff,

--- stuff-service/src/main/java/com/codecool/stuffservice/controller/StuffController.java
+++ stuff-service/src/main/java/com/codecool/stuffservice/controller/StuffController.java
@@ -1,12 +1,12 @@
 package com.codecool.stuffservice.controller;
 
 import com.codecool.stuffservice.entity.Stuff;
+import com.codecool.stuffservice.model.StuffDetailsResult;
+import com.codecool.stuffservice.model.StuffWithDetails;
+import com.codecool.stuffservice.service.StuffDetailsServiceCaller;
 import com.codecool.stuffservice.service.StuffProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,8 +18,14 @@ public class StuffController {
     @Autowired
     private StuffProvider stuffProvider;
 
+
     @GetMapping("/all")
     public List<Stuff> getAllStuff() {
         return stuffProvider.getAllStuff();
+    }
+
+    @GetMapping("/{stuff_id}")
+    public StuffWithDetails getStuffWithDetailsByStuffId(@PathVariable("stuff_id") Long stuffId) {
+        return stuffProvider.getStuffWithDetailsByStuffId(stuffId);
     }
 }
