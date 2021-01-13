d0bc7cd338137c986ab63473e80d731c43a9c89b
AnettKeszler <anett.dr.keszler@gmail.com>
2021-01-13 17:08:11 +0100

    add post feature to upload new stuff

--- garage-store-ui/src/components/Form.js
+++ garage-store-ui/src/components/Form.js
@@ -2,47 +2,40 @@ import React, {useEffect, useState} from 'react';
 import Axios from 'axios';
 
 function Form(props) {
-    // const [details, setDetails] = useState({});
-    // const [loaded, setLoaded] = useState(false);
-    // const [isNew, setIsNew] = useState(false);
     const id = props.match.params.id;
-    // const path = props.location.pathname;
-    // console.log(props)
 
-    // const addNewStuff = () => {
-    //     const
-    //
-    // }
+    const addNewStuff = () => {
+        const nameValue = document.querySelector("#name").value;
+        // alert("this was submitted: " + nameValue)
+        const priceValue = document.querySelector("#price").value;
+        // alert("this was submitted: " + priceValue);
+        const imageUrlValue = document.querySelector("#image").value;
+        const purchaseYearValue = document.querySelector("#year").value;
+        const descriptionValue = document.querySelector("#description").value;
+        const youTubeVideoUrlValue = document.querySelector("#youtubeVideoUrl").value;
 
+        const payload = {
+            "stuff": {
+                "name": nameValue,
+                "image": imageUrlValue,
+                "price": priceValue
+            },
+            "stuffDetailsResult": {
+                "purchaseYear": purchaseYearValue,
+                "description": descriptionValue,
+                "youtubeVideoUrl": youTubeVideoUrlValue
+            }
+        }
 
+        console.log(payload);
 
-    // useEffect(() => {
-    //     if (path.includes("update")) {
-    //         Axios.get(`http://localhost:8762/stuff/${id}`).then((response) => {
-    //             // const data = response.data;
-    //             setDetails(response.data);
-    //             setLoaded(true);
-    //             // console.log("data arrived", data);
-    //         });
-    //     }
-    // }, []);
-    //
-    // console.log(details);
-    // console.log(path.includes("update"));
-
-
-    // if (!loaded) {
-    //     return (<div>Please wait...</div>);
-    // }
-
-    // const { name, price, image } = details.stuff; // destruct
-    // const { stuffDetailsResult } = details;
-    // console.log("stuffdetailsresult", stuffDetailsResult);
-    // const {purchaseYear, description, youtubeVideoUrl } = stuffDetailsResult;
-
-    // const nameValue = isNew ? "" : name;
-    // console.log("nameValue", nameValue);
-
+        Axios.post(`http://localhost:8762/stuff/add`, payload).then(result => {
+            console.log(result);
+            console.log(result.data);
+        }).catch(error => {
+            console.log(error);
+        })
+    };
 
     return(
         <div style={formStyle}>
@@ -56,28 +49,27 @@ function Form(props) {
                 <br/>
                 <input type="text" id="price" name="price" placeholder="...what makes you happy." required="true"/>
                 <br/><br/>
-                <label htmlFor="image" >Upload an image about your product: </label>
+                <label htmlFor="image" >Copy an image url: </label>
                 <br/>
-                <textarea type="text" id="image" name="image" placeholder="http://image_example.com"/>
+                <textarea type="text" id="image" name="image" placeholder="http://image_example.com" required="true"/>
                 <br/><br/>
                 <label htmlFor="year" >When did you gain this stuff: </label>
                 <br/>
-                <input type="text" id="year" name="year" placeholder="e.g.: 2020..." />
+                <input type="text" id="year" name="year" placeholder="e.g.: 2020..." required="true"/>
                 <br/><br/>
                 <label htmlFor="description" >Description: </label>
                 <br/>
-                <textarea type="text" id="description" name="description" placeholder="..." />
+                <textarea type="text" id="description" name="description" placeholder="..." required="true" />
                 <br/><br/>
-                <label htmlFor="youtubeVideoUrl" >Upload video: </label>
+                <label htmlFor="youtubeVideoUrl" >Copy video url: </label>
                 <br/>
                 <textarea type="text" id="youtubeVideoUrl" name="youtubeVideoUrl" placeholder="Give a YouTube link to obtain more buyers..."/>
                 <br/><br/>
                 <input
                     type="button"
-                    value="Submit"/>
-                    {/*onClick={addNewStuff}*/}
+                    value="Submit"
+                    onClick={addNewStuff}/>
             </form>
-
         </div>
     );
 }
